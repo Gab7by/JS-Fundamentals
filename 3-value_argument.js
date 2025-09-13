d72e792a277e27a77e361767d9adecd8a6eb2474
Nana Kwame <owusugabriel803@gmail.com>
2025-09-13 16:26:56 +0000

    value_argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,7 @@
-const argue = 2;
-if (argue === 0){
-    console.log("No argument");
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else {
+  console.log(args[0]);
 }
